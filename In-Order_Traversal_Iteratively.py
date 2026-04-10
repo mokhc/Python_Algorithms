72cee87b16740eead9e7a1151b6983f1ce85bcf5
mokhc <mokhonchuen@gmail.com>
2026-04-10 21:39:24 +0800

    Add files via upload

---
+++ In-Order_Traversal_Iteratively.py
@@ -0,0 +1,80 @@
+# Test - In-Order Traversal Iteratively (left parent right ... root ... left parent right)
+# Set the in-order traversal values of a binary tree
+# @mokhc
+# 05/04/26
+from collections import deque
+from print_btree import print_btree
+
+# class BNode
+class BNode():
+	def __init__(self, v, left="", right=""):
+		self.val = v
+		self.left = left
+		self.right = right
+
+class Tra():
+	def in_tra1(self, input: BNode):
+		"""
+		:param input: input
+		:type input: BNode
+		:return: res1
+		:rtype: list
+		"""
+		# base
+		if type(input) == None:
+			return []
+		cur1 = input
+		res1 = []
+		stack1 = []  #
+		# loop to get values into the return list
+		while len(stack1) > 0 or cur1:
+			while cur1:
+				stack1.append(cur1)
+				cur1 = cur1.right
+			temp = stack1.pop()
+			res1.insert(0, temp.val)
+			cur1 = temp.left
+		return res1
+	
+	def in_tra2(self, input: BNode):
+		"""
+		:param input: input
+		:type input: BNode
+		:return: res1
+		:rtype: list
+		"""
+		# base
+		if type(input) != BNode:
+			return []
+		cur1 = input
+		res1 = []
+		stack1 = deque(list())
+		# loop to get values into the return list
+		while len(stack1) > 0 or cur1:
+			while cur1:
+				stack1.append(cur1)
+				cur1 = cur1.left
+			temp = stack1.pop()
+			res1.append(temp.val)
+			cur1 = temp.right
+		return res1
+
+# implementation 1
+t1 = BNode(1)
+t1.left = BNode(2)
+t1.right = BNode(3)
+t1.left.left = BNode(4)
+t1.left.right = BNode(5)
+t1.right.left = BNode(6)
+t1.right.right = BNode(7)
+print_btree(t1)
+
+print("----- 1 -----")  # [4, 2, 5, 1, 6, 3]
+s1 = Tra()
+print(s1.in_tra1(t1))
+print("----- 2 -----")
+s2 = Tra()
+print(s2.in_tra2(t1))
+
+# reference:
+# https://github.com/liyin2015/python-coding-interview/blob/master/Colab_Codes/chapter_tree_data_structure_and_traversal.ipynb
---
+++ Pre-Order_Traversal_Iteratively.py
@@ -0,0 +1,88 @@
+# Test - Pre-Order Traversal Iteratively (root ... middle left right ... middle left right
+# Set the pre-order traversal values of a binary tree
+# @mokhc
+# 10/04/26
+from collections import deque
+from print_btree import print_btree
+
+# class BNode
+class BNode():
+	def __init__(self, v, left="", right=""):
+		self.val = v
+		self.left = left
+		self.right = right
+
+class Tra():
+	def pre_tra2(self, input: BNode):
+		"""
+		:param input: input
+		:type input: BNode
+		:return: res1
+		:rtype: list
+
+        # key concept - using pop or popleft()
+        and setting the order in which the node value(s) are inserted into the return list
+        and using if conditions and append() or insert() to set the order in which the node(s)
+        are inserted into the deque or stack
+		"""
+		# base
+		if type(input) is None:
+			return []
+		res1 = []
+		stack1 = list([input])
+		# loop to get values into the return list
+		while len(stack1) > 0:
+			tmp1 = stack1.pop(0)
+			res1.append(tmp1.val)
+			if tmp1.right:
+				stack1.insert(0, tmp1.right)
+			if tmp1.left:
+				stack1.insert(0, tmp1.left)
+		return res1
+	
+	def pre_tra1(self, input: BNode):
+		"""
+		:param input: input
+		:type input: BNode
+		:return: res1
+		:rtype: list
+
+        # key concept - using pop or popleft()
+        and setting the order in which the node value(s) are inserted into the return list
+        and using if conditions and append() or insert() to set the order in which the node(s)
+        are inserted into the deque or stack
+		"""
+		# base
+		if type(input) is not BNode:
+			return []
+		res1 = []
+		stack1 = deque([input])
+		# loop to get values into the return list
+		while len(stack1) > 0:
+			tmp1 = stack1.popleft()
+			res1.append(tmp1.val)
+			if tmp1.right:
+				stack1.insert(0, tmp1.right)
+			if tmp1.left:
+				stack1.insert(0, tmp1.left)
+		return res1
+
+# implementation 1
+t1 = BNode(1)
+t1.left = BNode(2)
+t1.right = BNode(3)
+t1.left.left = BNode(4)
+t1.left.right = BNode(5)
+t1.right.left = BNode(6)
+t1.right.right = BNode(7)
+print_btree(t1)
+
+print("----- 1 -----")
+s1 = Tra()
+print(s1.pre_tra1(t1))
+print("----- 2 -----")
+s2 = Tra()
+print(s2.pre_tra2(t1))  # [1, 2, 4, 5, 3, 6, 7]
+
+# reference:
+# # https://github.com/liyin2015/python-coding-interview/blob/master/Colab_Codes/chapter_tree_data_structure_and_traversal.ipynb
